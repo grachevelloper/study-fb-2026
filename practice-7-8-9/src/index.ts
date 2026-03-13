e10378b4a4e31644c4f41851add11e95508d2c79
Nick Grachev <harizma.yt.2006@gmail.com>
2026-03-13 22:40:19 +0300

    add cors to practice-9

--- practice-7-8-9/src/index.ts
+++ practice-7-8-9/src/index.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response, NextFunction } from 'express';
 import swaggerUi from 'swagger-ui-express';
+import cors from 'cors';
 import routes from './routes';
 import { PORT } from './configs';
 import { swaggerSpec } from './swagger';
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
